aa68c11ef1bd4e512999a2b5e07f32c677b03661
AMilinovic0801 <amilinovic0801@gmail.com>
2024-02-08 22:41:59 +0100

    update

--- web-shop/js/header_hamburger.js
+++ web-shop/js/header_hamburger.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
+setTimeout(function() {
     // Hamburger icon click event
+    console.log('prvaprovjera');
     $("#hamburger").click(function() {
+        console.log('drugaprovjera');
         // Toggle the visibility of the navigation menu
         $("#nav-menu").slideToggle();
 });
-});
+}, 500);
